578daacdf95a8990998bb800f9516cb0575b90ac
Prudhvi Pulapa <95740635+prudviee@users.noreply.github.com>
2022-08-27 16:07:00 +0530

    Update MyContract.sol

--- MyContract.sol
+++ MyContract.sol
@@ -1,52 +1,58 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract MyContract {
-    struct Manufacturer {
+contract MyContract{
+
+
+    struct Manufacturer{
         string name;
         string supplierForTyres;
         string clientNameForBody;
     }
 
-    struct CarBodySupplier {
+
+    struct CarBodySupplier{
         string supplierName;
         //string supplierType;
     }
 
-    struct CarTyreSupplier {
+
+    struct CarTyreSupplier{
         string supplierName;
         //string supplierType;
         string suppliesTo;
     }
 
-    //variables used for actors
 
-    Manufacturer TataManufacturer;
-    Manufacturer MaruthiManufacturer;
+//variables used for actors
+
+
+    Manufacturer manufacturer;
 
     CarBodySupplier carBodySupplier;
+    CarTyreSupplier carTyreSupplier;
+    
 
-    CarTyreSupplier MrfCarTyreSupplier;
-    CarTyreSupplier CeatCarTyreSupplier;
+//variables for setting supply limits
 
-    //variables for setting supply limits
 
-    uint256 CarTyreSupplierLimit_MRF;
-    uint256 CarTyreSupplierLimit_CEAT;
-
+    uint CarTyreSupplierLimit_MRF;
+    uint CarTyreSupplierLimit_CEAT;
+    
     //uint CarBodySupplierLimit_VEDANTHA;
 
-    uint256 NumberOfCarBodiesNeeded_TATA = numberOfCarBodiesNeeded_TATA();
-    uint256 NumberOfCarBodiesNeeded_MARUTHI = numberOfCarBodiesNeeded_MARUTHI();
+    uint NumberOfCarBodiesNeeded_TATA = numberOfCarBodiesNeeded_TATA();
+    uint NumberOfCarBodiesNeeded_MARUTHI = numberOfCarBodiesNeeded_MARUTHI();
 
-    //Listing all the manufacturers and suppliers------------------------------------------------------
+//Listing all the manufacturers and suppliers------------------------------------------------------
+
 
     function setTataManufacturer() public {
-        TataManufacturer = Manufacturer("TATA", "MRF", "VEDANTA");
+        manufacturer = Manufacturer("TATA","MRF","VEDANTA");
     }
 
     function setMaruthiManufacturer() public {
-        MaruthiManufacturer = Manufacturer("MARUTHI", "CEAT", "VEDANTA");
+        manufacturer = Manufacturer("MARUTHI","CEAT","VEDANTA");
     }
 
     function setVedanthaSupplier() public {
@@ -54,39 +60,41 @@ contract MyContract {
     }
 
     function setMrfSupplier() public {
-        MrfCarTyreSupplier = CarTyreSupplier("MRF", "TATA");
+        carTyreSupplier = CarTyreSupplier("MRF","TATA");
     }
 
     function setCeatSupplier() public {
-        CeatCarTyreSupplier = CarTyreSupplier("CEAT", "MARUTHI");
+        carTyreSupplier = CarTyreSupplier("CEAT","MARUTHI");
     }
 
-    // Setting supplier limits for tyres------------------------------------------------------
-
-    function setCarTyreSupplierLimit_MRF(uint256 limit) public {
-        CarTyreSupplierLimit_MRF = limit;
+// Setting supplier limits for tyres------------------------------------------------------
+    
+    
+    function setCarTyreSupplierLimit_MRF(uint limit) public {
+        CarTyreSupplierLimit_MRF = limit ;
     }
 
-    function viewTyreSupplierLimit() public view returns (uint256) {
+    function viewTyreSupplierLimit() public view returns(uint){
         return CarTyreSupplierLimit_MRF;
     }
 
-    function setCarTyreSupplierLimit_CEAT(uint256 limit) public {
-        CarTyreSupplierLimit_CEAT = limit;
+    function setCarTyreSupplierLimit_CEAT(uint limit) public {
+        CarTyreSupplierLimit_CEAT = limit ;
     }
 
-    function viewCarTyreSupplierLimit_CEAT() public view returns (uint256) {
+    function viewCarTyreSupplierLimit_CEAT() public view returns(uint){
         return CarTyreSupplierLimit_CEAT;
     }
 
-    // car body counting------------------------------------------------------------
+    
+// car body counting------------------------------------------------------------
 
-    function numberOfCarBodiesNeeded_TATA() public view returns (uint256) {
-        return CarTyreSupplierLimit_MRF / 4;
-    }
+    function numberOfCarBodiesNeeded_TATA() public view returns (uint){
+        return CarTyreSupplierLimit_MRF/4; 
+    } 
 
-    function numberOfCarBodiesNeeded_MARUTHI() public view returns (uint256) {
-        return CarTyreSupplierLimit_CEAT / 4;
+    function numberOfCarBodiesNeeded_MARUTHI() public view returns (uint){
+        return CarTyreSupplierLimit_CEAT/4; 
     }
 
     /*function carBodySupplyLimit_VEDANTHA(uint carBodies) public {
@@ -100,20 +108,22 @@ contract MyContract {
 
 //Bidding contract------------------------------------------------------------------
 
-contract BidForCarBodies is MyContract {
-    enum BidResult {
-        WON,
-        LOST
-    }
+contract BidForCarBodies is MyContract{
+
+    enum BidResult{ WON, LOST}
     BidResult tataResult;
     BidResult maruthiResult;
 
-    struct Bid {
-        uint256 quantity;
-        uint256 amount;
+    struct Bid{
+        uint quantity;
+        uint amount;
     }
 
-    //placing for bids
+    uint carBodiesWonInAuction_TATA = 0;
+    uint carBodiesWonInAuction_MARUTHI = 0;
+
+//placing for bids
+
 
     Bid CarBodySupplierBid_VEDANTHA;
     Bid CarTyreSupplierBid_MRF;
@@ -123,161 +133,130 @@ contract BidForCarBodies is MyContract {
     Bid CarTyreManufacturerBid_TATA;
     Bid CarTyreManufacturerBid_MARUTHI;
 
-    //Supplier Bid Functions --------------------- Input ---------------------- And Outputs -----------------------
-    function carBodySupplierBid(uint256 quantity, uint256 amount) public {
-        CarBodySupplierBid_VEDANTHA = Bid(quantity, amount);
+
+//Supplier Bid Functions --------------------- Input ---------------------- And Outputs -----------------------
+    function carBodySupplierBid (uint quantity,uint amount) public {
+        CarBodySupplierBid_VEDANTHA =Bid(quantity,amount);
     }
 
-    function viewCarBodySupplierBid() public view returns (Bid memory) {
+    function viewCarBodySupplierBid () public view returns(Bid memory){
         return CarBodySupplierBid_VEDANTHA;
     }
 
-    function carTyreSupplierBid_MRF(uint256 amount) public {
-        CarTyreSupplierBid_MRF = Bid(CarTyreSupplierLimit_MRF, amount);
+
+    function carTyreSupplierBid_MRF (uint amount) public {
+        CarTyreSupplierBid_MRF =Bid(CarTyreSupplierLimit_MRF,amount);
     }
 
-    function viewCarTyreSupplierBid_MRF() public view returns (Bid memory) {
+    function viewCarTyreSupplierBid_MRF () public view returns(Bid memory){
         return CarTyreSupplierBid_MRF;
     }
 
-    function carTyreSupplierBid_CEAT(uint256 amount) public {
-        CarTyreSupplierBid_CEAT = Bid(CarTyreSupplierLimit_CEAT, amount);
+
+    function carTyreSupplierBid_CEAT (uint amount) public {
+        CarTyreSupplierBid_CEAT =Bid(CarTyreSupplierLimit_CEAT,amount);
     }
 
-    function viewCarTyreSupplierBid_CEAT() public view returns (Bid memory) {
+    function viewCarTyreSupplierBid_CEAT () public view returns(Bid memory){
         return CarTyreSupplierBid_CEAT;
     }
 
-    //Manufacturer Bid Functions --------------------- Input ---------------------- And Outputs -----------------------
 
-    function carBodyManufacturerBid_TATA(uint256 quantity, uint256 amount)
-        public
-    {
-        CarBodyManufacturerBid_TATA = Bid(quantity, amount);
+//Manufacturer Bid Functions --------------------- Input ---------------------- And Outputs -----------------------
+
+
+    function carBodyManufacturerBid_TATA (uint quantity,uint amount) public {
+        CarBodyManufacturerBid_TATA =Bid(quantity,amount);
     }
 
-    function viewCarBodyManufacturerBid_TATA()
-        public
-        view
-        returns (Bid memory)
-    {
+    function viewCarBodyManufacturerBid_TATA () public view returns(Bid memory){
         return CarBodyManufacturerBid_TATA;
     }
 
-    function carBodyManufacturerBid_MARUTHI(uint256 quantity, uint256 amount)
-        public
-    {
-        CarBodyManufacturerBid_MARUTHI = Bid(quantity, amount);
+
+    function carBodyManufacturerBid_MARUTHI (uint quantity,uint amount) public {
+        CarBodyManufacturerBid_MARUTHI = Bid(quantity,amount);
     }
 
-    function viewCarBodyManufacturerBid_MARUTHI()
-        public
-        view
-        returns (Bid memory)
-    {
+    function viewCarBodyManufacturerBid_MARUTHI () public view returns(Bid memory){
         return CarBodyManufacturerBid_MARUTHI;
     }
 
-    //Bidding Function ---------------------------------------------
-    function biddingForCarBody() public {
-        if (
-            CarBodyManufacturerBid_TATA.quantity >
-            CarBodySupplierBid_VEDANTHA.quantity &&
-            CarBodyManufacturerBid_MARUTHI.quantity >
-            CarBodySupplierBid_VEDANTHA.quantity
-        ) {
+//Bidding Function ---------------------------------------------
+    function biddingForCarBody() public  {
+        
+        if(CarBodyManufacturerBid_TATA.quantity > CarBodySupplierBid_VEDANTHA.quantity && CarBodyManufacturerBid_MARUTHI.quantity > CarBodySupplierBid_VEDANTHA.quantity){
             tataResult = BidResult.LOST;
             maruthiResult = BidResult.LOST;
-        } else {
-            if (
-                CarBodyManufacturerBid_MARUTHI.quantity >
-                CarBodySupplierBid_VEDANTHA.quantity &&
-                CarBodyManufacturerBid_TATA.quantity <
-                CarBodySupplierBid_VEDANTHA.quantity
-            ) {
+            carBodiesWonInAuction_TATA = 0;
+            carBodiesWonInAuction_MARUTHI = 0;
+        }
+        else{
+            if(CarBodyManufacturerBid_MARUTHI.quantity > CarBodySupplierBid_VEDANTHA.quantity && CarBodyManufacturerBid_TATA.quantity <= CarBodySupplierBid_VEDANTHA.quantity){
                 tataResult = BidResult.WON;
                 maruthiResult = BidResult.LOST;
-            } else if (
-                CarBodyManufacturerBid_MARUTHI.quantity <
-                CarBodySupplierBid_VEDANTHA.quantity &&
-                CarBodyManufacturerBid_TATA.quantity >
-                CarBodySupplierBid_VEDANTHA.quantity
-            ) {
+                carBodiesWonInAuction_TATA = CarBodyManufacturerBid_TATA.quantity;
+                carBodiesWonInAuction_MARUTHI = 0;
+            }
+        
+            else if (CarBodyManufacturerBid_MARUTHI.quantity <= CarBodySupplierBid_VEDANTHA.quantity && CarBodyManufacturerBid_TATA.quantity > CarBodySupplierBid_VEDANTHA.quantity){
                 tataResult = BidResult.LOST;
                 maruthiResult = BidResult.WON;
-            } else if (
-                CarBodyManufacturerBid_MARUTHI.quantity <
-                CarBodySupplierBid_VEDANTHA.quantity &&
-                CarBodyManufacturerBid_TATA.quantity <
-                CarBodySupplierBid_VEDANTHA.quantity
-            ) {
-                if (
-                    CarBodyManufacturerBid_MARUTHI.quantity >
-                    NumberOfCarBodiesNeeded_MARUTHI &&
-                    CarBodyManufacturerBid_TATA.quantity >
-                    NumberOfCarBodiesNeeded_MARUTHI
-                ) {
-                    //check the algorithm for this-------------------------------------------------------------------------------------------------------
-                    if (
-                        CarBodyManufacturerBid_MARUTHI.amount >
-                        CarBodyManufacturerBid_TATA.quantity
-                    ) {
+                carBodiesWonInAuction_TATA = 0;
+                carBodiesWonInAuction_MARUTHI = CarBodyManufacturerBid_MARUTHI.quantity;
+            }
+        
+            else if (CarBodyManufacturerBid_MARUTHI.quantity < CarBodySupplierBid_VEDANTHA.quantity && CarBodyManufacturerBid_TATA.quantity < CarBodySupplierBid_VEDANTHA.quantity){
+            
+                if(CarBodyManufacturerBid_MARUTHI.quantity > NumberOfCarBodiesNeeded_MARUTHI && CarBodyManufacturerBid_TATA.quantity > NumberOfCarBodiesNeeded_TATA){
+ //check the algorithm for this-------------------------------------------------------------------------------------------------------               
+                    if(CarBodyManufacturerBid_MARUTHI.amount > CarBodyManufacturerBid_TATA.quantity){
                         tataResult = BidResult.LOST;
                         maruthiResult = BidResult.WON;
-                    } else {
+                        carBodiesWonInAuction_TATA = CarBodySupplierBid_VEDANTHA.quantity - CarBodyManufacturerBid_MARUTHI.quantity;
+                        carBodiesWonInAuction_MARUTHI = CarBodyManufacturerBid_MARUTHI.quantity;
+                    }
+                    else{
                         tataResult = BidResult.WON;
                         maruthiResult = BidResult.LOST;
-                    }
-                } else if (
-                    CarBodyManufacturerBid_MARUTHI.quantity >
-                    NumberOfCarBodiesNeeded_MARUTHI &&
-                    CarBodyManufacturerBid_TATA.quantity ==
-                    NumberOfCarBodiesNeeded_MARUTHI
-                ) {
+                        carBodiesWonInAuction_TATA = CarBodyManufacturerBid_TATA.quantity;
+                        carBodiesWonInAuction_MARUTHI = CarBodySupplierBid_VEDANTHA.quantity - CarBodyManufacturerBid_TATA.quantity;
+                    }}
+                else if (CarBodyManufacturerBid_MARUTHI.quantity > NumberOfCarBodiesNeeded_MARUTHI && CarBodyManufacturerBid_TATA.quantity == NumberOfCarBodiesNeeded_TATA){
                     tataResult = BidResult.WON;
                     maruthiResult = BidResult.LOST;
-                } else if (
-                    CarBodyManufacturerBid_MARUTHI.quantity ==
-                    NumberOfCarBodiesNeeded_MARUTHI &&
-                    CarBodyManufacturerBid_TATA.quantity >
-                    NumberOfCarBodiesNeeded_MARUTHI
-                ) {
+                    carBodiesWonInAuction_TATA = CarBodyManufacturerBid_TATA.quantity;
+                    carBodiesWonInAuction_MARUTHI = CarBodySupplierBid_VEDANTHA.quantity - CarBodyManufacturerBid_TATA.quantity;
+                }
+                else if (CarBodyManufacturerBid_MARUTHI.quantity == NumberOfCarBodiesNeeded_MARUTHI && CarBodyManufacturerBid_TATA.quantity > NumberOfCarBodiesNeeded_TATA){
                     tataResult = BidResult.LOST;
                     maruthiResult = BidResult.WON;
-                } else if (
-                    (CarBodyManufacturerBid_MARUTHI.quantity ==
-                        NumberOfCarBodiesNeeded_MARUTHI &&
-                        CarBodyManufacturerBid_TATA.quantity ==
-                        NumberOfCarBodiesNeeded_MARUTHI) ||
-                    (CarBodyManufacturerBid_MARUTHI.quantity <
-                        NumberOfCarBodiesNeeded_MARUTHI &&
-                        CarBodyManufacturerBid_TATA.quantity ==
-                        NumberOfCarBodiesNeeded_MARUTHI) ||
-                    (CarBodyManufacturerBid_MARUTHI.quantity ==
-                        NumberOfCarBodiesNeeded_MARUTHI &&
-                        CarBodyManufacturerBid_TATA.quantity <
-                        NumberOfCarBodiesNeeded_MARUTHI) ||
-                    (CarBodyManufacturerBid_MARUTHI.quantity <
-                        NumberOfCarBodiesNeeded_MARUTHI &&
-                        CarBodyManufacturerBid_TATA.quantity <
-                        NumberOfCarBodiesNeeded_MARUTHI)
-                ) {
-                    if (
-                        CarBodyManufacturerBid_MARUTHI.amount >
-                        CarBodyManufacturerBid_TATA.quantity
-                    ) {
-                        tataResult = BidResult.LOST;
-                        maruthiResult = BidResult.WON;
-                    } else {
-                        tataResult = BidResult.WON;
-                        maruthiResult = BidResult.LOST;
-                    }
+                    carBodiesWonInAuction_TATA = CarBodySupplierBid_VEDANTHA.quantity - CarBodyManufacturerBid_MARUTHI.quantity;
+                    carBodiesWonInAuction_MARUTHI = CarBodyManufacturerBid_MARUTHI.quantity;
+                }
+                else if ((CarBodyManufacturerBid_MARUTHI.quantity == NumberOfCarBodiesNeeded_MARUTHI && CarBodyManufacturerBid_TATA.quantity == NumberOfCarBodiesNeeded_TATA) || 
+                        (CarBodyManufacturerBid_MARUTHI.quantity < NumberOfCarBodiesNeeded_MARUTHI && CarBodyManufacturerBid_TATA.quantity == NumberOfCarBodiesNeeded_TATA) ||
+                        (CarBodyManufacturerBid_MARUTHI.quantity == NumberOfCarBodiesNeeded_MARUTHI && CarBodyManufacturerBid_TATA.quantity < NumberOfCarBodiesNeeded_TATA) ||
+                        (CarBodyManufacturerBid_MARUTHI.quantity < NumberOfCarBodiesNeeded_MARUTHI && CarBodyManufacturerBid_TATA.quantity < NumberOfCarBodiesNeeded_TATA))
+                        {
+                            if(CarBodyManufacturerBid_MARUTHI.amount > CarBodyManufacturerBid_TATA.quantity){
+                                tataResult = BidResult.LOST;
+                                maruthiResult = BidResult.WON;
+                                carBodiesWonInAuction_TATA = CarBodySupplierBid_VEDANTHA.quantity - CarBodyManufacturerBid_MARUTHI.quantity;
+                                carBodiesWonInAuction_MARUTHI = CarBodyManufacturerBid_MARUTHI.quantity;
+                            }
+                            else{
+                                tataResult = BidResult.WON;
+                                maruthiResult = BidResult.LOST;
+                                carBodiesWonInAuction_TATA = CarBodyManufacturerBid_TATA.quantity;
+                                carBodiesWonInAuction_MARUTHI = CarBodySupplierBid_VEDANTHA.quantity - CarBodyManufacturerBid_TATA.quantity;
+                            }    
+                        }
                 }
             }
-        }
+         
     }
-
-    function viewBidResult() public view returns (BidResult, BidResult) {
-        return (tataResult, maruthiResult);
+    function viewBidResult() public view returns(BidResult,BidResult){
+        return (tataResult,maruthiResult);
     }
 }
